eea8edfa64e882dcca95f95ad57b30d2e7aac98c
Anthony Corcutt <acorcutt@gmail.com>
2021-04-30 21:43:28 +0100

    strange build type error

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 
 export default function Header({ host }) {
+  const localhost = String(host).includes('localhost');
   return (
     <div>
-      <Link href={host.includes('localhost') ? 'http://localhost:3000/' : 'https://next-multi-host.vercel.app/'}>Host 1</Link>
+      <Link href={localhost ? 'http://localhost:3000/' : 'https://next-multi-host.vercel.app/'}>Host 1</Link>
       {' - '}
-      <Link href={host.includes('localhost') ? 'http://another.localhost:3000/' : 'https://next-multi-host-acorcutt.vercel.app/'}>Host 2</Link>
+      <Link href={localhost ? 'http://another.localhost:3000/' : 'https://next-multi-host-acorcutt.vercel.app/'}>Host 2</Link>
     </div>
   );
 }
