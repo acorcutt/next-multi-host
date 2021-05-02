3f0f317e62928a1ba9881ae7d34913f118187d17
Anthony Corcutt <acorcutt@gmail.com>
2021-05-02 16:13:25 +0100

    move catchall from root

--- next.config.js
+++ next.config.js
@@ -42,8 +42,8 @@ module.exports = {
               value: '(?<host>.*)',
             },
           ],
-          source: '/catchall/:path*',
-          destination: '/hosts/:host/catchall/:path*',
+          source: '/:part/:path*',
+          destination: '/hosts/:host/catchall/:part/:path*',
         },
       ],
     };
