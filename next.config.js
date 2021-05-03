d5a9a42fa129bb3fe2800b3abf18d302e1eb4c04
Anthony Corcutt <acorcutt@gmail.com>
2021-05-03 14:19:05 +0100

    use fallback rewrite

--- next.config.js
+++ next.config.js
@@ -3,37 +3,41 @@ module.exports = {
     webpack5: false,
   },
   async rewrites() {
-    return [
-      {
-        has: [
-          {
-            type: 'host',
-            value: '(?<host>.*)',
-          },
-        ],
-        source: '/',
-        destination: '/hosts/:host',
-      },
-      {
-        has: [
-          {
-            type: 'host',
-            value: '(?<host>.*)',
-          },
-        ],
-        source: '/posts/:id',
-        destination: '/posts/:id/hosts/:host',
-      },
-      {
-        has: [
-          {
-            type: 'host',
-            value: '(?<host>.*)',
-          },
-        ],
-        source: '/:slug*',
-        destination: 'https://next-multi-host.vercel.app/hosts/:host/slugs/:slug*',
-      },
-    ];
+    return {
+      afterFiles: [
+        {
+          has: [
+            {
+              type: 'host',
+              value: '(?<host>.*)',
+            },
+          ],
+          source: '/',
+          destination: '/hosts/:host',
+        },
+        {
+          has: [
+            {
+              type: 'host',
+              value: '(?<host>.*)',
+            },
+          ],
+          source: '/posts/:id',
+          destination: '/posts/:id/hosts/:host',
+        },
+      ],
+      fallback: [
+        {
+          has: [
+            {
+              type: 'host',
+              value: '(?<host>.*)',
+            },
+          ],
+          source: '/:slug*',
+          destination: 'https://next-multi-host.vercel.app/hosts/:host/slugs/:slug*',
+        },
+      ],
+    };
   },
 };
