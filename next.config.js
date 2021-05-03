5a694f267cb9ec68cd6954212317e8f88786c3ab
Anthony Corcutt <acorcutt@gmail.com>
2021-05-03 14:42:30 +0100

    use proxy hack for catch all

--- next.config.js
+++ next.config.js
@@ -3,62 +3,77 @@ module.exports = {
     webpack5: false,
   },
   async rewrites() {
-    return [
-      {
-        has: [
-          {
-            type: 'host',
-            value: '(?<host>.*)',
-          },
-        ],
-        source: '/',
-        destination: '/hosts/:host',
-      },
-      {
-        has: [
-          {
-            type: 'host',
-            value: '(?<host>.*)',
-          },
-          {
-            type: 'header',
-            key: 'accept-encoding',
-            value: '(?<encoding>.*)',
-          },
-          {
-            type: 'header',
-            key: 'cache-control',
-            value: '(?<cache>.*)',
-          },
-          // {
-          //   type: 'header',
-          //   key: 'user-agent',
-          //   value: '(?<agent>.*)',
-          // },
-        ],
-        source: '/headers',
-        destination: '/hosts/:host/headers/:encoding---:cache',
-      },
-      {
-        has: [
-          {
-            type: 'host',
-            value: '(?<host>.*)',
-          },
-        ],
-        source: '/posts/:id',
-        destination: '/hosts/:host/posts/:id',
-      },
-      {
-        has: [
-          {
-            type: 'host',
-            value: '(?<host>.*)',
-          },
-        ],
-        source: '/slugs/:slug*',
-        destination: '/hosts/:host/slugs/:slug*',
-      },
-    ];
+    return {
+      afterFiles: [
+        {
+          has: [
+            {
+              type: 'host',
+              value: '(?<host>.*)',
+            },
+          ],
+          source: '/',
+          destination: '/hosts/:host',
+        },
+        {
+          has: [
+            {
+              type: 'host',
+              value: '(?<host>.*)',
+            },
+            {
+              type: 'header',
+              key: 'accept-encoding',
+              value: '(?<encoding>.*)',
+            },
+            {
+              type: 'header',
+              key: 'cache-control',
+              value: '(?<cache>.*)',
+            },
+            // {
+            //   type: 'header',
+            //   key: 'user-agent',
+            //   value: '(?<agent>.*)',
+            // },
+          ],
+          source: '/headers',
+          destination: '/hosts/:host/headers/:encoding---:cache',
+        },
+        {
+          has: [
+            {
+              type: 'host',
+              value: '(?<host>.*)',
+            },
+          ],
+          source: '/posts/:id',
+          destination: '/hosts/:host/posts/:id',
+        },
+        {
+          has: [
+            {
+              type: 'host',
+              value: '(?<host>.*)',
+            },
+          ],
+          source: '/slugs/:slug*',
+          destination: '/hosts/:host/slugs/:slug*',
+        },
+      ],
+      //Hack to support catch-all routes until supported by proxy back to itself
+      fallback: [
+        {
+          has: [
+            {
+              type: 'host',
+              value: '(?<host>.*)',
+            },
+          ],
+          source: '/:slug*',
+          destination: 'https://next-multi-host.vercel.app/hosts/:host/slugs/:slug*',
+        },
+      ],
+    };
   },
 };
